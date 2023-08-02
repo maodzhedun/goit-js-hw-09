72f99161a0e498d6e8ec463cca85ffe61e07df95
maodzhedun <maodzhedun@gmail.com>
2023-08-02 22:41:18 +0300

    First task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
-console.log("switch");
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    if (timerId !== null) {
+      startBtn.disabled = true;
+    } body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stoptBtn.addEventListener("click", () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+  });
