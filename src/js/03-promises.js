74b811909a291b76a57dd5aa4a0c6d76ab3cd9b2
maodzhedun <maodzhedun@gmail.com>
2023-08-07 07:25:28 +0300

    add 3 part task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,35 @@
 import Notiflix from "notiflix";
 
+const form = document.querySelector('.form');
+form.addEventListener('submit,', onSubmit);
+console.dir(form);
+
+function onSubmit(event) {
+
+}
+
+
+
 
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  setTimeout(() => {
+    if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
+  }, delay)
+  
 }
+
+
+// createPromise(position, delay)
+//   .then(({ position, delay }) => {
+//     Notiflix.Report.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Report.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
